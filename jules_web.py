c69b3a6dbbb1090dab602d98885609a16e43aaf1
hpreston <hank.preston@gmail.com>
2014-12-14 23:52:53 -0500

    Some basic framing of the client layout.  Using some sample Jinja templates for the layout.  No real work done as simultaneously building out the jules_app apis needed to support.

--- jules_web.py
+++ jules_web.py
@@ -1,13 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
 
+@app.route("/")
+def template_test():
+    return render_template('template.html', my_string="Wheeee!", my_list=[0,1,2,3,4,5], title="Home")
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route("/status")
+def status():
+    return render_template('template.html', my_string="Wheeee!", my_list=[0,1,2,3,4,5], title="Status")
+
+@app.route("/inventory")
+def inventory():
+    # ToDo - working on updating jules_app for the needed api calls for this to work
+    # ToDo - need to build out the template for the display of returned data
+    return render_template('inventory.html', title="Inventory")
+
+@app.route("/schedule")
+def schedule():
+    return render_template('template.html', my_string="Wheeee!", my_list=[0,1,2,3,4,5], title="Schedule")
+
+@app.route("/notify")
+def notify():
+    return render_template('template.html', my_string="Wheeee!", my_list=[0,1,2,3,4,5], title="Notify")
+
+@app.route("/settings")
+def settings():
+    return render_template('template.html', my_string="Wheeee!", my_list=[0,1,2,3,4,5], title="Settings")
 
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=8080)
